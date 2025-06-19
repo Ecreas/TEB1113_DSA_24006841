ea93abcf89d6a04d1f6897290e438f65b66bca05
Ecreas <140377885+Ecreas@users.noreply.github.com>
2025-06-19 17:28:40 +0800

    Add files via upload

---
+++ 24006841_Najmi_L4/24006841_L4_DoublyCircular.cpp
@@ -0,0 +1,63 @@
+// 24006841 Najmi G2
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    string name;
+    Node* next;
+    Node* prev;
+    Node(string name) : name(name) , next(nullptr) {}
+
+    
+
+};
+
+class CircularLinkedList {
+public:
+
+string name;
+Node* head = nullptr;
+Node* tails = nullptr;
+
+    void addNode(string name) {
+        Node* newNode = new Node(name);
+        if (head == nullptr) {
+            head = newNode;
+            tails = newNode;
+            newNode->next = head; 
+            newNode->prev = tails; 
+        } else {
+            tails->next = newNode;
+            tails = newNode;
+            tails->next = head; 
+        }
+    }
+
+    void display() {
+        Node* current = head;
+
+       do {
+        cout << current->name << "-> ";
+        current = current->next;
+    } while (current != head);
+cout << current->name << endl;
+
+}
+};
+
+int main() {
+
+
+CircularLinkedList cll;
+
+cll.addNode("Ali");
+cll.addNode("Bob"); 
+cll.addNode("Cade");
+
+cll.display();
+
+
+    return 0;
+
+}
