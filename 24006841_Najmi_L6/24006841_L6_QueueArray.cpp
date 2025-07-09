9fb0859feb869644832d4ef9eaba1e6d7c8b0954
Ecreas <140377885+Ecreas@users.noreply.github.com>
2025-07-09 19:23:19 +0800

    Add files via upload

---
+++ 24006841_Najmi_L6/24006841_L6_QueueArray.cpp
@@ -0,0 +1,68 @@
+//24006841 G2
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Queue {
+public:
+    string names[50];  
+    int front = 0;
+    int rear = -1;
+    int count = 0;
+
+    void enqueue(string value) {
+        if (count == 50) {
+            cout << "Queue full!" << endl;
+            return;
+        }
+        rear = (rear + 1);  
+        names[rear] = value;
+        count++;
+    }
+
+    void dequeue() {
+        if (count == 0) {
+            cout << "Queue empty!" << endl;
+            return;
+        }
+        front = (front + 1);  
+        count--;
+    }
+
+    string peek() {
+        if (count == 0) return "nothing in queue";
+        return names[front];
+    }
+
+    void display() {
+        if (count == 0) {
+            cout << "notning in queue" << endl;
+            return;
+        }
+        for (int i = 0; i < count; i++) {
+            int index = (front + i);
+            cout << names[index];
+            if (i < count - 1) cout << " -> ";
+        }
+        cout << " -> NULL" << endl;
+    }
+};
+
+int main() {
+    Queue q;
+    
+    q.enqueue("Ali");
+    q.enqueue("Bob");
+    q.enqueue("Cade");
+    q.display(); 
+    
+    cout << "Peek: " << q.peek() << endl;  
+    
+    q.dequeue();
+    q.display(); 
+    
+    q.dequeue();
+    q.dequeue();
+    q.display();  
+}
